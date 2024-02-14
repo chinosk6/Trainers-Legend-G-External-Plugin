8afd5aef8a25fc30861adbd12ed259f39aae00ff
Re*Index. (ot_inc) <32851879+reindex-ot@users.noreply.github.com>
2024-02-15 02:23:45 +0900

    Add ja_JP

---
+++ gui/qtui/ts_ja_jp/main_ui_ja_jp.ts
@@ -0,0 +1,82 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja_JP">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="main_ui.py" line="99"/>
+        <source>Trainers-Legend-G External Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="100"/>
+        <source>Config Settings</source>
+        <translation type="unfinished">設定</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="101"/>
+        <source>Discord RPC</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="102"/>
+        <source>Game Fast Login</source>
+        <translation type="unfinished">高速でログイン (DMMのアクティベーションなし)</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="103"/>
+        <source>Game Fast Reboot</source>
+        <translation type="unfinished">高速で再起動</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="105"/>
+        <source>Plugin Version</source>
+        <translation type="unfinished">プラグインのバージョン</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="106"/>
+        <source>version_now</source>
+        <translation type="unfinished">現在のバージョン</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="107"/>
+        <source>version_latest</source>
+        <translation type="unfinished">最新のバージョン</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="108"/>
+        <source>Update</source>
+        <translation type="unfinished">アップデート</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="109"/>
+        <source>About</source>
+        <translation type="unfinished">バージョン情報</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="110"/>
+        <source>Github - Trainers-Legend-G</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="111"/>
+        <source>Github - This Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="104"/>
+        <source>Reload Config and Textdb</source>
+        <translation type="unfinished">設定とテキストを再読み込み</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="130"/>
+        <source>Change API</source>
+        <translation type="unfinished">APIの変更</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="131"/>
+        <source>Check Update</source>
+        <translation type="unfinished">アップデートを確認</translation>
+    </message>
+</context>
+</TS>
---
+++ gui/qtui/ts_ja_jp/window_settings.ts
@@ -0,0 +1,157 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS []>
+<TS version="2.1" language="ja_JP">
+  <context>
+    <name>MainWindow</name>
+    <message>
+      <location filename="window_settings.py" line="291" />
+      <source>Window Settings</source>
+      <translation type="unfinished">ウィンドウの設定</translation>
+    </message>
+    <message>
+      <location filename="window_settings.py" line="292" />
+      <source>Window Info</source>
+      <translation type="unfinished">ウィンドウの情報</translation>
+    </message>
+    <message>
+      <location filename="window_settings.py" line="293" />
+      <source>Class Name</source>
+      <translation type="unfinished">クラス名</translation>
+    </message>
+    <message>
+      <location filename="window_settings.py" line="294" />
+      <source>Move Window</source>
+      <translation type="unfinished">ウィンドウを移動</translation>
+    </message>
+    <message>
+      <location filename="window_settings.py" line="295" />
+      <source>↑</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <location filename="window_settings.py" line="296" />
+      <source>←</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <location filename="window_settings.py" line="297" />
+      <source>→</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <location filename="window_settings.py" line="298" />
+      <source>↓</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <location filename="window_settings.py" line="299" />
+      <source>Move Step</source>
+      <translation type="unfinished">移動のステップ</translation>
+    </message>
+    <message>
+      <location filename="window_settings.py" line="300" />
+      <source>20</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <location filename="window_settings.py" line="301" />
+      <source>Update Data</source>
+      <translation type="unfinished">データのアップデート</translation>
+    </message>
+    <message>
+      <location filename="window_settings.py" line="302" />
+      <source>Refresh</source>
+      <translation type="unfinished">更新</translation>
+    </message>
+    <message>
+      <location filename="window_settings.py" line="303" />
+      <source>Sync pos to right</source>
+      <translation type="unfinished">ウィンドウの位置を右に同期する</translation>
+    </message>
+    <message>
+      <location filename="window_settings.py" line="304" />
+      <source>Window Name</source>
+      <translation type="unfinished">ウィンドウ名</translation>
+    </message>
+    <message>
+      <location filename="window_settings.py" line="305" />
+      <source>UnityWndClass</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <location filename="window_settings.py" line="306" />
+      <source>umamusume</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <location filename="window_settings.py" line="307" />
+      <source>Window Position</source>
+      <translation type="unfinished">ウィンドウの位置</translation>
+    </message>
+    <message>
+      <location filename="window_settings.py" line="325" />
+      <source>w</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <location filename="window_settings.py" line="314" />
+      <source>Unknown</source>
+      <translation type="unfinished">不明</translation>
+    </message>
+    <message>
+      <location filename="window_settings.py" line="326" />
+      <source>x</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <location filename="window_settings.py" line="324" />
+      <source>y</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <location filename="window_settings.py" line="327" />
+      <source>h</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <location filename="window_settings.py" line="316" />
+      <source>Resize Window</source>
+      <translation type="unfinished">ウィンドウをリサイズ</translation>
+    </message>
+    <message>
+      <location filename="window_settings.py" line="317" />
+      <source>Horizontal</source>
+      <translation type="unfinished">水平</translation>
+    </message>
+    <message>
+      <location filename="window_settings.py" line="328" />
+      <source>Keep Ratio</source>
+      <translation type="unfinished">比率を維持</translation>
+    </message>
+    <message>
+      <location filename="window_settings.py" line="323" />
+      <source>Vertical</source>
+      <translation type="unfinished">垂直</translation>
+    </message>
+    <message>
+      <location filename="window_settings.py" line="329" />
+      <source>Aspect Ratio:</source>
+      <translation type="unfinished">アスペクト比：</translation>
+    </message>
+    <message>
+      <location filename="window_settings.py" line="330" />
+      <source>16.0000 : 9.0000</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <location filename="window_settings.py" line="331" />
+      <source>Config</source>
+      <translation type="unfinished">設定</translation>
+    </message>
+    <message>
+      <location filename="window_settings.py" line="332" />
+      <source>Update Window Position</source>
+      <translation type="unfinished">アップデートウィンドウの位置</translation>
+    </message>
+  </context>
+</TS>
